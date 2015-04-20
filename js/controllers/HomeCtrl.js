0a58cd84520fef9d9089b68b97f6ef6a85a8776f
jayesh.makhija <jayesh.makhija@indexexchange.com>
2015-04-20 10:01:03 -0400

    Landing Page

--- js/controllers/HomeCtrl.js
+++ js/controllers/HomeCtrl.js
@@ -1,4 +1,13 @@
 angular.module('prototype').controller('HomeCtrl', ['$scope', function($scope) {
-    $scope.test = "Hello world";
+    $scope.user="Jayesh Makhija";
+    $scope.prodServer = "17th March 2015";
+    $scope.ip = "1.1.7.160";
+    $scope.myServer = "7th February 2015";
+
+    $scope.pushPlans = [
+    {name : "CPAUCBU-26", date : "17th March 2015"},
+    {name : "CPAUCBU-13", date : "5th March 2015"},
+    {name : "CPAUCBU-10", date : "7th February 2015"},
+    {name : "CPAUCBU-09", date : "2nd January 2015"}];
 
 }]);
