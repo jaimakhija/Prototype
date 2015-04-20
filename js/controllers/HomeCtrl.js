b3ac12f2d38c2d2388f2bc941c7f0b018d9ff829
jayesh.makhija <jayesh.makhija@indexexchange.com>
2015-04-20 10:16:29 -0400

    Landing Page -2

--- js/controllers/HomeCtrl.js
+++ js/controllers/HomeCtrl.js
@@ -4,10 +4,14 @@ angular.module('prototype').controller('HomeCtrl', ['$scope', function($scope) {
     $scope.ip = "1.1.7.160";
     $scope.myServer = "7th February 2015";
 
+    $scope.tableParams = new ngTableParams(parameter, settings);
     $scope.pushPlans = [
     {name : "CPAUCBU-26", date : "17th March 2015"},
     {name : "CPAUCBU-13", date : "5th March 2015"},
     {name : "CPAUCBU-10", date : "7th February 2015"},
     {name : "CPAUCBU-09", date : "2nd January 2015"}];
 
+    getData: function ($defer, params){
+    	$defer.resolve(pushPlans);
+    }
 }]);
